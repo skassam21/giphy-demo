89bdaa18cd4836a656d4c8af8660906c76ac46cb
Shums Kassam <skassam21@gmail.com>
2018-06-06 12:10:25 -0400

    add tempaltes

--- js/app.js
+++ js/app.js
@@ -12,6 +12,7 @@ const searchForm = () => {
 
     searchGifs(searchQuery)
     .then((response) => {
+        // now response is a object in javascript
         clearResults();
         if (response.data && response.data.length > 0) {
             addGifs(response.data);
@@ -58,27 +59,36 @@ const searchGifs = (searchQuery) => {
         q: searchQuery
     });
 
+    // url = https://api.giphy.com/v1/gifs/search?api_key=sdfsdfsfsdf&q=
+
     return fetch(url, {mode: 'cors'})
     .then((response) => {
         return response.json()
     });
 }
 
+// Check out: http://handlebarsjs.com/
+// Check out: https://wesbos.com/template-strings-html/
 
 const addGifs = (gifs) => {
-    const resultsElem = document.createElement('div');
-    resultsElem.classList.add("row");
+    const resultsStr = `
+        <div class="row">
+            ${gifs.map(gif => createGifElement(gif)).join('')}
+        </div>
+    `;
 
-    for (let i in gifs) {
-        const gif = gifs[i];
-        const gifDiv = document.createElement("div");
-        gifDiv.className = "col-12 col-sm-6 col-md-4 col-lg-3";
-        const gifElem = document.createElement("img");
-        gifElem.className = "gif";
-        gifElem.src = gif.images.original.url;
-        
-        gifDiv.appendChild(gifElem);
-        resultsElem.appendChild(gifDiv);
-    }
-    results.appendChild(resultsElem);
+    results.innerHTML = resultsStr;
+}
+
+
+const createGifElement = (gif) => {
+    const gifStr = `<div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                        <div class="gif-container">
+                            <img src="${gif.images.downsized.url}" class="gif"/>
+                            <p class="text-center">${gif.slug}</p>
+                        </div>
+                    </div>`;
+
+    return gifStr;
+
 }
